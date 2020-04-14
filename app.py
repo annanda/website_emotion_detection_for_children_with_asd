530d391fdcbe5fb44b4be7363095d04b46da0e69
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-04-14 17:51:34 +0100

    Adding website pages and filling Researchers page.

--- app.py
+++ app.py
@@ -26,5 +26,30 @@ def elements_page():
     return render_template('elements.html')
 
 
+@app.route('/about_project', methods=['GET'])
+def project_page():
+    return render_template('project.html')
+
+
+@app.route('/contact', methods=['GET'])
+def contact_page():
+    return render_template('contact.html')
+
+
+@app.route('/data_protection_ethics', methods=['GET'])
+def ethics_page():
+    return render_template('data_protection_ethics.html')
+
+
+@app.route('/researchers', methods=['GET'])
+def researchers_page():
+    return render_template('researchers.html')
+
+
+@app.route('/information', methods=['GET'])
+def information_page():
+    return render_template('documents.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9090)
