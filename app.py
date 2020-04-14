0fa36c78b28a9453742ddc40eb8c0bd523a4145b
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-04-14 14:06:21 +0100

    Applying HTML/CSS template.

--- app.py
+++ app.py
@@ -7,8 +7,23 @@ app = Flask(__name__, template_folder="./templates")
 @app.route('/', methods=['GET'])
 def index():
     info = {}
-    return "Hello World"
-    # return render_template('index.html', **info)
+    return render_template('index.html', **info)
+
+
+@app.route('/index', methods=['GET'])
+def index_page():
+    info = {}
+    return render_template('index.html', **info)
+
+
+@app.route('/generic', methods=['GET'])
+def generic_page():
+    return render_template('generic.html')
+
+
+@app.route('/elements', methods=['GET'])
+def elements_page():
+    return render_template('elements.html')
 
 
 if __name__ == '__main__':
